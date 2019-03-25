90c48d7ad20f53b6dceb3e6082edac86fe6850eb
Brianna Kincart <briannakincart@gmail.com>
2019-03-25 14:32:06 -0400

    Update data and stylesheets

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import data from './constants/data';
-import App from './components/app'
+import App from './components/App'
 
 ReactDOM.render(
   <App data={data}/>, document.getElementById('app')
