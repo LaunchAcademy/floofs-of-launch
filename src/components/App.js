b296b98ab977cb3fe1ca991bb9defa7558e81860
dovakeen118 <kegillis118@gmail.com>
2019-12-23 20:40:02 -0500

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import DogSection from "./DogSection"
+import CatSection from "./CatSection"
+
 const App = props => {
+
   return(
-    <h1>Place Fluffballs here</h1>
+    <div className="app">
+      <h1>Place Fluffballs Here</h1>
+
+      <DogSection
+        dogsInfo={props.animals.dogs}
+      />
+
+      <CatSection
+        catsInfo={props.animals.cats}
+      />
+    </div>
   )
 }
 
