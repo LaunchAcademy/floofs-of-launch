5877b47824209465e86a397727b0433ceb6417d9
Kaylyn DiCiaccio <kaylyndiciaccio@kaylyns-air.lan>
2021-03-24 17:21:40 -0400

    added finished code and notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import DogSection from "./DogSection"
 import CatTile from "./CatTile"
 
 const App = (props) => {
-  // let catComponents = []
+  //this useState is for selected and unselect single floofs
+  const [favoriteFloof, setFavoriteFloof] = useState(null)
 
-  // props.animals.cats.forEach((cat) => {
-  //   catComponents.push(<CatTile />)
-  // }) 
+  //this useState is for selected and unselect a group of floofs
+  // const [selectedFloofs, setSelectedFloofs] = useState([])
 
   const catComponents = props.animals.cats.map((catObject) => {
+
+    // this is how you set up a single floof to be clicked
+    let selectedStatus = false
+    if (favoriteFloof === catObject.id) {
+      selectedStatus = true
+    }
+
+    const handleClick = () => {
+      if (selectedStatus === false) {
+        setFavoriteFloof(catObject.id)
+      } else {
+        setFavoriteFloof(null)
+      }
+    }
+
+    // this is a handleClick for an array of floofs
+    // the top part stores all status as false, but when the selectedFloofs includes the catObject.id that was clicked on, it will change status to true
+
+    // let selectedStatus = false
+    // if (selectedFloofs.includes(catObject.id))  {
+    //   selectedStatus = true
+    // }
+
+    // In this handlClick if the selectedFloofs includes the clicked catObject.id, then you want to filter out that catObject.id from your selectedFloofs, otherwise if it's not there add it to selectedFloofs
+    
+    // const handleClick = () => {
+    //   if (selectedFloofs.includes(catObject.id)) {
+    //     setSelectedFloofs(selectedFloofs.filter(cat => cat != catObject.id))
+    //   } else {
+    //     setSelectedFloofs(selectedFloofs.concat(catObject.id))
+    //   }
+    // }
+
     return(
       <CatTile
         key={catObject.id}
@@ -18,13 +51,15 @@ const App = (props) => {
         personality={catObject.personality}
         human={catObject.human}
         image={catObject.image}
+        handleClick={handleClick}
+        selectedStatus={selectedStatus}
       />
     )
   })
   
   return(
-    <div>
-      <DogSection dogsArray={props.animals.dogs} />
+    <div className="app">
+      
       {catComponents}
     </div>
   )
@@ -33,7 +68,7 @@ const App = (props) => {
 export default App;
 
 
-
+{/* <DogSection dogsArray={props.animals.dogs} /> */}
 
 
 //  <CatTile cats={props.animals.cats[0]} />
--- src/components/CatTile.js
+++ src/components/CatTile.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const CatTile = (props) => {
+
+// this creates a variable that stores a class which you add to the className in the div 
+  let classes = "container"
+  // if the selected status is true, then we append the class background-change to the class variable
+  if (props.selectedStatus === true) {
+    classes += " background-change"
+  }
+
+
   return (
-    <div>
+    <div className={classes} onClick={props.handleClick}>
       <h1>{props.name}</h1>
       <p>{props.personality}</p>
       <p>{props.human}</p>
