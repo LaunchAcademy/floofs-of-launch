9a4affbca8bb4aa3cb77f8964a076da0bda24419
Nick Alberts <nwalberts5@gmail.com>
2023-10-04 13:04:53 -0400

    done with part 1

--- src/components/ChinchillaComponent.js
+++ src/components/ChinchillaComponent.js
@@ -1,16 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
 const ChinchillaComponent = (props) => {
-  
-  let chinchillaImage = <img src="https://4.bp.blogspot.com/-9TSGZyRfGOo/UdzF-ATNl6I/AAAAAAAAD0Y/V7r3PoHAV8k/s400/Funny-chinchilla-Animal.jpg" />
 
+  const [chinchillaVisibility, setChinchillaVisibility] = useState(false)
+
+  const toggleChinch = () => {
+    setChinchillaVisibility(!chinchillaVisibility)
+  }
+
+  let chinchillaImage 
+  if (chinchillaVisibility === true) {
+    chinchillaImage = <img src="https://4.bp.blogspot.com/-9TSGZyRfGOo/UdzF-ATNl6I/AAAAAAAAD0Y/V7r3PoHAV8k/s400/Funny-chinchilla-Animal.jpg" />
+  }
 
   return (
     <div>
       {chinchillaImage}
 
-      <button>
-        TOGGLE JAZZ
+      <button onClick={toggleChinch}>
+        TOGGLE JAZZY CHINCHILLA
       </button>
     </div>
   )
--- src/components/FluffyFriends.js
+++ src/components/FluffyFriends.js
@@ -1,5 +1,5 @@
-import React from "react"
-// import ChinchillaComponent from "./ChinchillaComponent"
+import React, { useState } from "react"
+import ChinchillaComponent from "./ChinchillaComponent"
 
 // basic event management with alert/log
 // hug the teddybear to increase hug count
@@ -9,16 +9,38 @@ import React from "react"
 // time permitting, we can refactor teddybear into its own component too
 
 const FluffyFriends = (props) => {
+  
+  const [hugCount, changeHugCount] = useState(0)
 
+  const triggerWhenClicked = () => {
+    changeHugCount(hugCount + 1)
+  }
+
+  let bobIsContent 
+  if (hugCount >= 10){
+    bobIsContent = "Aw gee thanks y'all"
+  }
+
+  // let chinchilla
+  // if (chinchillaVisibility === true) {
+  //   chinchilla = <ChinchillaComponent />
+  // }
+ 
   return (
     <div>
       <h1>This bear needs encouragement....</h1>
+      <h3>{props.saying}</h3>
 
       <img
+        onClick={triggerWhenClicked}
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQROkYb-EUzzOgzBB_b0K4SaLpsefVjgRKfZMuhBTtOUlnIL6gPOC27FdMIjjklUWcQHGU3iYE&usqp=CAc"
       />
 
-      <h3>Hug Count:0</h3>
+      {bobIsContent}
+
+      <h3> Hug Count: {hugCount} </h3>
+
+      <ChinchillaComponent />
     </div>
   )
 }
